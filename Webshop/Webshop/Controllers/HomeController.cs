8c6cd779b8223bd8e861944f20d313effef57eed
Simon Bothén <caresilabs@gmail.com>
2015-05-06 13:54:27 +0200

    Woop woop db is up

--- Webshop/Webshop/Controllers/HomeController.cs
+++ Webshop/Webshop/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,12 +10,102 @@ namespace Webshop.Controllers
 {
     public class HomeController : Controller
     {
+        private bool connection_open;
+        private MySqlConnection connection;
+
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
+
+            Get_Connection();
+
+
+            try
+            {
+
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandText =
+            string.Format("select * from Anstalld where AnstID = '{0}'",
+                                              1);
+
+                //cmd.
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                try
+                {
+                    reader.Read();
+                    String Name;
+                    if (reader.IsDBNull(1) == false)
+                        Name = reader.GetString(1);
+                    else
+                        Name = null;
+
+                    ViewBag.Message = Name;
+
+                    reader.Close();
+
+                }
+                catch (MySqlException e)
+                {
+                    string MessageString = "Read error occurred  / entry not found loading the Column details: "
+                        + e.ErrorCode + " - " + e.Message + "; \n\nPlease Continue";
+                 
+                }
+            }
+            catch (MySqlException e)
+            {
+                string MessageString = "The following error occurred loading the Column details: "
+                    + e.ErrorCode + " - " + e.Message;
+               
+            }
+
+
+
+
+            connection.Close();
+
+            //MySqlCommand 
             return View();
         }
+
+        private void Get_Connection()
+        {
+            connection_open = false;
+
+            connection = new MySqlConnection();
+            //connection = DB_Connect.Make_Connnection(ConfigurationManager.ConnectionStrings["SQLConnection"].ConnectionString);
+            connection.ConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            //            if (db_manage_connnection.DB_Connect.OpenTheConnection(connection))
+            if (Open_Local_Connection())
+            {
+                connection_open = true;
+            }
+            else
+            {
+                //					MessageBox::Show("No database connection connection made...\n Exiting now", "Database Connection Error");
+                //					 Application::Exit();
+            }
+
+        }
+
+        private bool Open_Local_Connection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
 
         public ActionResult About()
         {
